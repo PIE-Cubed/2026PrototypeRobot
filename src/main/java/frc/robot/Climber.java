9eea8b787baf76cc9eaf84579b8be11b6d68e12a
Kepler <robolions2199@gmail.com>
2026-02-01 14:01:50 -0500

    added climber class

---
+++ src/main/java/frc/robot/Climber.java
@@ -0,0 +1,33 @@
+package frc.robot;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
+
+
+public class Climber {
+ // Motor IDs
+    private final int CLIMB_MOTOR_ID = 51;
+
+
+    public Climber()  {
+
+    }
+
+
+
+    /********************************************************************
+     * 
+     * TEST PROGRAMS
+     * 
+     ********************************************************************/
+
+     public void testMotor()  {
+        
+     }
+}
